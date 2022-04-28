3ea5381993fa7628509e13f5a1db24843514d3b5
Billy <billyjcatchpole@gmail.com>
2022-04-28 10:54:19 +0100

    chore: replace mongo container with cloud connection string

--- services/auth/src/routes/__test__/signout.test.ts
+++ services/auth/src/routes/__test__/signout.test.ts
@@ -1,7 +1,7 @@
 import request from "supertest";
 import app from "../../app";
 
-it("returns correct response on successful signin", async () => {
+it("returns correct response when user signs out", async () => {
   const res = await request(app).get("/api/users/signout").expect(200);
 
   expect(res.body.currentUser).toBeNull();
