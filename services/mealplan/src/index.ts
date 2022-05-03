91f9e1673220b61640a7eac251b323aa7830ffd7
Billy <billyjcatchpole@gmail.com>
2022-05-03 16:17:06 +0100

    feat: connect mealplan to nats

--- services/mealplan/src/index.ts
+++ services/mealplan/src/index.ts
@@ -1,5 +1,7 @@
+import { AccountCreatedListener } from "./events/listeners/account-created-listener";
 import mongoose from "mongoose";
 import app from "./app";
+import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -10,8 +12,37 @@ const start = async () => {
     throw new Error("Kube variable MONGO_URI must be defined");
   }
 
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("Kube variable NATS_CLUSTER_ID must be defined");
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("Kube variable NATS_CLIENT_ID must be defined");
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error("Kube variable NATS_URL must be defined");
+  }
+
   try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL,
+    );
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+
+    new AccountCreatedListener(natsWrapper.client).listen();
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI);
+
     app.listen(3000, () => {
       console.log("Mealplan running on 3000");
     });
