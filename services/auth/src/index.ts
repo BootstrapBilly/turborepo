2878683b7411b19b74794d249b564243588d8f97
Billy <billyjcatchpole@gmail.com>
2022-05-03 14:10:13 +0100

    feat: add nats connection to auth service

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -1,3 +1,4 @@
+import { natsWrapper } from "./natswrapper";
 import mongoose from "mongoose";
 import app from "./app";
 
@@ -10,7 +11,33 @@ const start = async () => {
     throw new Error("Kube variable MONGO_URI must be defined");
   }
 
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("Kube variable NATS_CLUSTER_ID must be defined");
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("Kube variable NATS_CLIENT_ID must be defined");
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error("Kube variable NATS_URL must be defined");
+  }
+
   try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL,
+    );
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI);
     app.listen(3000, () => {
       console.log("Auth running on 3000");
