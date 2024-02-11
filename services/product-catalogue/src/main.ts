55d5b204087142fc9677aa75b2435cc60f030dd2
billy <billyjcatchpole@gmail.com>
2024-02-11 10:59:20 +0000

    delete old services

--- services/product-catalogue/src/main.ts
+++ services/product-catalogue/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,19 @@ async function bootstrap() {
       return new BadRequestException(errorsForResponse);
     },
   }));
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Product catalogue')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
