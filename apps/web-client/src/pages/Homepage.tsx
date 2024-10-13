f041b81818c52fe4f50d5b1150bb2c6504071193
billy <billyjcatchpole@gmail.com>
2024-10-13 09:30:02 +0100

    import backend type

--- apps/web-client/src/pages/Homepage.tsx
+++ apps/web-client/src/pages/Homepage.tsx
@@ -1,13 +1,21 @@
-import { useQuery } from "@tanstack/react-query"
-import { productApi } from "../api/product-catalogue"
+import { useQuery } from "@tanstack/react-query";
+import { Product } from "@billy-catchpole/product-catalogue-open-api-client";
+import { productApi } from "../api/product-catalogue";
+
+export const car: Product = {
+  description: "A car",
+  id: "1",
+  name: "Bmw",
+  price: 80000,
+};
 
 export const Homepage = () => {
-    const productQuery = useQuery({
-        queryKey: ['products'],
-        queryFn: () => productApi.listProducts(),
-        select: ({ data }) => data
-    })
+  const productQuery = useQuery({
+    queryKey: ["products"],
+    queryFn: () => productApi.listProducts(),
+    select: ({ data }) => data,
+  });
 
-    console.log(productQuery.data)
-    return (<div>hello, good evening</div>)
-}
+  console.log(productQuery.data);
+  return <div>hello, good evening</div>;
+};
